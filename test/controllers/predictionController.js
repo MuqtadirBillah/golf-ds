44a2cfca57f2d96582bbc42cb33d9425ccc56d04
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-12-22 15:25:32 +0500

    golf ds

--- test/controllers/predictionController.js
+++ test/controllers/predictionController.js
@@ -37,7 +37,7 @@ const trainData = ()=>{
 }
 
 const predictData = (req, res)=>{
-    console.log(trainData());
+    console.table(trainData());
     result = net.run([1,0]);
     console.log(result)
     res.send(`Hello ${result}`)
