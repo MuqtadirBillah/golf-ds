835c18d28c9e20814539ccb80d362e263190c01c
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-12-22 15:28:11 +0500

    golf ds

--- test/controllers/predictionController.js
+++ test/controllers/predictionController.js
@@ -37,8 +37,27 @@ const trainData = ()=>{
 }
 
 const predictData = (req, res)=>{
-    console.table(trainData());
-    result = net.run([1,0]);
+    const results = [];
+    fs.createReadStream('./dataset/golf.csv')
+    .pipe(csv())
+    .on('data', (data) =>{
+        console.log(data)
+        results.push(data)
+    })
+    .on('end', () => {
+        // [
+            //   { NAME: 'Daffy Duck', AGE: '24' },
+            //   { NAME: 'Bugs Bunny', AGE: '22' }
+            // ]
+    });
+    
+    net.train([
+        { input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 } },
+        { input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 } },
+        { input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 } },
+    ]);
+
+    let result = net.run([1,0]);
     console.log(result)
     res.send(`Hello ${result}`)
 }
