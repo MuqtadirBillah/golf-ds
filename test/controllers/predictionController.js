f5ccea4101535f75805f599b71a41eea68828785
MuqtadirBillah <abbasimuqtadir@gmail.com>
2022-12-22 16:00:56 +0500

    golf ds

--- test/controllers/predictionController.js
+++ test/controllers/predictionController.js
@@ -55,8 +55,8 @@ const predictData = (req, res)=>{
             console.log(typeof results)
             let trainingData = []
             results.forEach(r=>{
-                let status = (r.Play=='yes') ? 1 : 2
-                predictions.push({ Play: (r.Play=='yes') ? 1 : 2 })
+                let status = (r.Play=='yes') ? 1 : 0
+                predictions.push({ Play: (r.Play=='yes') ? 1 : 0 })
                 delete r.Play;
                 delete r['Play'];
                 console.table(r)
